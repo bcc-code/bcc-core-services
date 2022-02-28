759989e945fba61f459cc1004945863af0fd4bfe
Piotr <czpiotr@gmail.com>
2022-02-28 10:41:51 +0100

    Initialize Dapper with Sql connection

--- BuildingBlocks/dotnet/BuildingBlocks/Sql/DapperExtensions.cs
+++ BuildingBlocks/dotnet/BuildingBlocks/Sql/DapperExtensions.cs
@@ -1,16 +1,17 @@
 using Dapper;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BuildingBlocks.Sql
 {
 
     public static class DapperExtensions
     {
-        public static void AddConverters()
+        public static void InitializeDapper(this IServiceCollection services, string connectionString)
         {
             SqlMapper.AddTypeHandler(new MemberIdConverter());
             SqlMapper.AddTypeHandler(new DateTimeConverter());
-
+            
+            services.AddScoped<ISqlConnectionService, SqlConnectionService>(x => new SqlConnectionService(connectionString));
         }
-
     }
 }
