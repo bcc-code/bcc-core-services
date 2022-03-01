cc6dccc704f28e52d4b4f38b155711fd42cbaaad
Piotr <czpiotr@gmail.com>
2022-03-01 13:55:12 +0100

    Updated package versions to 5 (.NET 5)

--- samples/WebApi/Startup.cs
+++ samples/WebApi/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BuildingBlocks.Api.Authentication;
+using BuildingBlocks.Api.OpenApi;
 using BuildingBlocks.Sql;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace WebApi
@@ -40,11 +35,16 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseBccSwagger(new OpenApiOptions
+            {
+                ApiTitle = "WebApi v1",
+                ApiVersion = "/swagger/v1/swagger.json",
+                AuthenticationType = WebAuthenticationType.Auth0
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+                
             }
 
             app.UseHttpsRedirection();
