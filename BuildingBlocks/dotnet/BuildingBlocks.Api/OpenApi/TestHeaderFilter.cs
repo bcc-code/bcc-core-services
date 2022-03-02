bb180cfdfcd437ed284963cdee0e3d37c6e84910
Rafal Cypcer <cypcer.rafal@gmail.com>
2022-03-02 10:41:00 +0100

    [CHANGE] - add Test AuthenticationType

---
+++ BuildingBlocks/dotnet/BuildingBlocks.Api/OpenApi/TestHeaderFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace BuildingBlocks.Api.OpenApi
+{
+    public class TestHeaderFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+            {
+                operation.Parameters = new List<OpenApiParameter>();
+            }
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "AW-User-Id",
+                In = ParameterLocation.Header,
+                Schema = new OpenApiSchema {Type = "number"},
+                Required = true
+            });
+        }
+    }
+}
