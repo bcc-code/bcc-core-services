c2a3903d6dec40241bdbd4cccb52c253568f4217
Jakub Czyz <jakub12134@gmail.com>
2022-04-05 12:57:55 +0200

    Initial commit

---
+++ bcc-orgs/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bcc-orgs/src/router"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	r := gin.Default()
+	router.LoadRoutes(r)
+
+	r.Run()
+}
