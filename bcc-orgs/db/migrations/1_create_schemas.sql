0c614afa9402b53b26ad3404159d3b11448cb056
Steven Malaihollo <stevenjanm@gmail.com>
2022-04-21 09:55:12 +0200

    add Org SQL schema

--- bcc-orgs/db/migrations/1_create_schemas.sql
+++ bcc-orgs/db/migrations/1_create_schemas.sql
@@ -1,5 +1,23 @@
-CREATE TABLE orgs(
-   name varchar,
+CREATE TABLE address (
+   addressID int,
+   PRIMARY KEY(addressID),
+   street1 varchar,
+   street2 varchar,
+   city varchar,
+   region varchar,
+   countryIso2Code varchar,
+   postalCode varchar,
+   countryName varchar,
+   countryNameNative varchar
+);
+
+CREATE TABLE org (
    orgID int,
-   PRIMARY KEY(orgID)
+   PRIMARY KEY(orgID),
+   name varchar,
+   legalName varchar NULL,
+   type varchar,
+   fk_visitingAddressID int references address (addressID),
+   fk_postalAddressID int references address (addressID),
+   fk_billingAddressID int references address (addressID)
 );
--- bcc-orgs/db/migrations/2_add_test_data.sql
+++ bcc-orgs/db/migrations/2_add_test_data.sql
@@ -1,2 +1,5 @@
-INSERT INTO orgs (name, orgID)
-VALUES ('Terwolde',96),('Oslo/Follo',69);
+INSERT INTO address (addressID, street1, street2, city, region, countryIso2Code, postalCode, countryName, countryNameNative)
+VALUES (1, 'Street', 'Street2', 'Apeldoorn', 'Gelderland', 'NL', '7322EE', 'Netherlands', 'Nederland');
+
+INSERT INTO org (name, orgID, fk_visitingAddressID)
+VALUES ('Terwolde', 96, 1), ('Oslo/Follo', 69, NULL);
