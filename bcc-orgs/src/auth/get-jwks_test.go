e9ee5bfacabff9709a5ddae76f1a7af42b2aab1a
Jakub Czyz <jakub12134@gmail.com>
2022-04-27 13:20:09 +0200

    update action flow and fix test

--- bcc-orgs/src/auth/get-jwks_test.go
+++ bcc-orgs/src/auth/get-jwks_test.go
@@ -1,12 +1,9 @@
 package auth
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGetJWKS(t *testing.T) {
-	keySet := GetKeySet(auth0Issuer)
-	panic("Whatever")
-	fmt.Printf("%+v\n", keySet)
+	GetKeySet(auth0Issuer)
 }
