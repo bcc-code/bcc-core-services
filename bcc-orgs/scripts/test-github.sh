22380dd81e845bdc17c9a036404f899fcde100a1
Jakub Czyz <jakub12134@gmail.com>
2022-04-27 14:50:50 +0200

    update test setup

--- bcc-orgs/scripts/test-github.sh
+++ bcc-orgs/scripts/test-github.sh
@@ -1,3 +1,2 @@
 #!/bin/bash 
-docker-compose -f ${BASH_SOURCE%/*}/../devops/test.docker-compose.yml -p "orgs-api-test" up -d --build
-${BASH_SOURCE%/*}/test-docker.sh
+docker-compose -f ${BASH_SOURCE%/*}/../devops/test.docker-compose.yml run --rm orgs_api_test
