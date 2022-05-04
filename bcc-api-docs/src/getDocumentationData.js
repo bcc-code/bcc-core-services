424c4dac932cbf4f22790b524c7e8f0d985479f8
Jakub Czyz <jakub12134@gmail.com>
2022-05-04 14:47:36 +0200

    update documentation env var name

--- bcc-api-docs/src/getDocumentationData.js
+++ bcc-api-docs/src/getDocumentationData.js
@@ -1,6 +1,6 @@
 
 export default () => {
-    const documentationData = process.env.DOCUMENTATION_DATA
+    const documentationData = process.env.DOCUMENTATION_URLS
     console.log(documentationData)
     if(!documentationData) return []
     return JSON.parse(documentationData)
