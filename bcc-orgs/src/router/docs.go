6627d843d1df5f4e722eae57bd103307f667fbe9
Jakub Czyz <jakub12134@gmail.com>
2022-05-05 10:06:32 +0200

    update docs route

--- bcc-orgs/src/router/docs.go
+++ bcc-orgs/src/router/docs.go
@@ -8,7 +8,7 @@ import (
 
 func LoadDocs(r *gin.Engine) {
 	doc := docs.SwaggerInfo.ReadDoc()
-	r.GET("/swagger.json", func(c *gin.Context) {
+	r.GET("orgs/swagger.json", func(c *gin.Context) {
 		c.String(200, doc)
 	})
 }
